dd984e037ae10cb6e0ab31c1d6a3734d221ee5d0
Pawel Smolarski <smolenc1@gmail.com>
2022-08-31 00:11:12 +0200

    pizzeria-javascript

--- src/js/script.js
+++ src/js/script.js
@@ -52,11 +52,24 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  class Product{
+    constructor(){
+      const thisProduct = this;
+      console.log('new Product:', thisProduct);
+    }
+  }
   const app = {
     initMenu: function(){
-      const testProduct = new Product();
-      console.log('testProduct:', testProduct);
+      const thisApp = this;
+      console.log('thisApp.data:', thisApp.data);
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
 
+    },
+    initData: function(){
+      const thisApp = this;
+      thisApp.data = dataSource;
     },
     init: function(){
       const thisApp = this;
@@ -65,7 +78,7 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
-
+      thisApp.initData();
       thisApp.initMenu();
     },
   };
